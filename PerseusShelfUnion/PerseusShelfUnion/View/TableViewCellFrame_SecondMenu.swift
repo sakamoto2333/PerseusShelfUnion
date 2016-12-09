b6ef3535b81e75f36287fbd9d0965cebeefdc8b0
Silver Wei <silveracgal@gmail.com>
2016-12-09 17:01:42 +0800

    滑稽

--- PerseusShelfUnion/PerseusShelfUnion/View/TableViewCellFrame_SecondMenu.swift
+++ PerseusShelfUnion/PerseusShelfUnion/View/TableViewCellFrame_SecondMenu.swift
@@ -21,7 +21,7 @@ class TableViewCellFrame_SecondMenu: UIView {
     }
     
     func showFrame() {
-        let TopLine = UIView(frame: CGRect(x: 20, y: 0, width: self.frame.size.width - (20 * 2), height: 1))
+        let TopLine = UIView(frame: CGRect(x: 8, y: 0, width: self.frame.size.width - (8 * 2), height: 1))
         let LetfLine = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: self.frame.size.height))
         let RightLine = UIView(frame: CGRect(x: self.frame.size.width, y: 0, width: 1, height: frame.size.height))
         
