1da021610bd8cde38c71b01a69449280442a4401
Silver Wei <silveracgal@gmail.com>
2016-12-14 10:19:36 +0800

    添加.h头文件，添加ProgressHUD

--- PerseusShelfUnion/PerseusShelfUnion/Other/ErrorMessages.swift
+++ PerseusShelfUnion/PerseusShelfUnion/Other/ErrorMessages.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class ErrorMessages: NSObject {
+    var ErrorCode = [Int:String]()
+    override init() {
+        ErrorCode[0x1001] = "请输入用户名和密码"
+    }
+    
+    
+    func ApiResult(errorCode: Int) {
+        ProgressHUD.showError(ErrorCode[errorCode])
+    }
+}
