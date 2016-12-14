d3566fcfcd4dfa668c87b821753a0bc2138ba606
sakamoto2333 <kk15052943800@gmail.com>
2016-12-14 16:28:01 +0800

    滑稽

---
+++ PerseusShelfUnion/PerseusShelfUnion/Controller/L_LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  PerseusShelfUnion
+//
+//  Created by 123 on 16/12/14.
+//  Copyright © 2016年 XHVolunteeriOS. All rights reserved.
+//
+
+import UIKit
+
+class L_LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func L_back(segue:UIStoryboardSegue) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
