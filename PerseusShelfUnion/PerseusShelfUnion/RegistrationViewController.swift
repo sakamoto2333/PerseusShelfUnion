7b8304be4b2991eee6395b956f09a7bf64bdf859
sakamoto2333 <kk15052943800@gmail.com>
2016-12-14 08:59:35 +0800

    验证码

---
+++ PerseusShelfUnion/PerseusShelfUnion/RegistrationViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RegistrationViewController.swift
+//  PerseusShelfUnion
+//
+//  Created by 123 on 16/12/14.
+//  Copyright © 2016年 XHVolunteeriOS. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var generateCodeView: GenerateCodeView!
+    @IBOutlet weak var CodeTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func register(_ sender: AnyObject) {
+        if generateCodeView.codeString.lowercased() == CodeTextField.text?.lowercased() {
+            print("1")
+        }else {
+            print("2")
+        }
+        CodeTextField.resignFirstResponder()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
