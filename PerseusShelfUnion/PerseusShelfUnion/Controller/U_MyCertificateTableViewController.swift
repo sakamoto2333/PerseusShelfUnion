09d16e2f86fca6b2e77484a8ba263bdf77f4b8e6
sakamoto2333 <kk15052943800@gmail.com>
2016-12-20 16:28:45 +0800

    资质认证

--- PerseusShelfUnion/PerseusShelfUnion/Controller/U_MyCertificateTableViewController.swift
+++ PerseusShelfUnion/PerseusShelfUnion/Controller/U_MyCertificateTableViewController.swift
@@ -8,88 +8,141 @@
 
 import UIKit
 
-class U_MyCertificateTableViewController: UITableViewController {
+class U_MyCertificateTableViewController: UITableViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var Image1: UIImageView!
+    @IBOutlet weak var Image2: UIImageView!
+    @IBOutlet weak var Image3: UIImageView!
+    @IBOutlet weak var Image4: UIImageView!
+    @IBOutlet weak var Image5: UIImageView!
+    @IBOutlet weak var Image6: UIImageView!
+    @IBOutlet weak var Image7: UIImageView!
+    @IBOutlet weak var Image8: UIImageView!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var view5: UIView!
+    @IBOutlet weak var view6: UIView!
+    @IBOutlet weak var view7: UIView!
+    @IBOutlet weak var view8: UIView!
+    var picker = UIImagePickerController()
+    let alert:UIAlertController=UIAlertController(title: "选择图片", message: nil, preferredStyle: .actionSheet)
+    var images: UIImageView!
+    var views: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        getimage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func getimage() {
+        picker.delegate = self
+        alert.addAction(UIAlertAction(title: "相机拍摄", style: .default, handler: { (UIAlertAction) in
+            self.openCamera()
+        }))
+        alert.addAction(UIAlertAction(title: "相册", style: .default, handler: { (UIAlertAction) in
+            self.openGallary()
+        }))
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
+    }
+    
+    func openCamera() {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            self.present(picker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alertwarning = UIAlertController(title: "警告", message: "你没有相机", preferredStyle: .alert)
+            alertwarning.addAction(UIAlertAction(title: "好的", style: .cancel, handler: nil))
+            self.present(alertwarning, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallary() {
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let imageget = info[UIImagePickerControllerOriginalImage] as? UIImage
+        images.image = imageget
+        self.views.backgroundColor = UIColor(red: 230/255, green: 56/255, blue: 58/255, alpha: 1)
+        images.isHidden = false
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 8
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func Qbutton1(_ sender: Any) {
+        if Image1.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image1
+            views = view1
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @IBAction func Qbutton2(_ sender: Any) {
+        if Image2.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image2
+            views = view2
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    @IBAction func Qbutton3(_ sender: Any) {
+        if Image3.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image3
+            views = view3
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    @IBAction func Qbutton4(_ sender: Any) {
+        if Image4.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image4
+            views = view4
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    @IBAction func Qbutton5(_ sender: Any) {
+        if Image5.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image5
+            views = view5
+        }
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func Qbutton6(_ sender: Any) {
+        if Image6.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image6
+            views = view6
+        }
     }
-    */
-
+    @IBAction func Qbutton7(_ sender: Any) {
+        if Image7.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image7
+            views = view7
+        }
+    }
+    @IBAction func Qbutton8(_ sender: Any) {
+        if Image8.isHidden == true {
+            self.present(alert, animated: true, completion: nil)
+            images = Image8
+            views = view8
+        }
+    }
 }
