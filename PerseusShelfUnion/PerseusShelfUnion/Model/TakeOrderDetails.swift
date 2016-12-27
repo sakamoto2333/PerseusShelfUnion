f435b1eaeb4125015f970152f2f69199264461c7
sakamoto2333 <kk15052943800@gmail.com>
2016-12-27 11:01:39 +0800

    报价和抢单合并 (#15)
    * 添加分支
    
    * 搞事情
    
    * 小问题
    
    * aaa
    
    * 无可奉告
    
    * 上传图片
    
    * 修复键盘bug
    
    * 再简化一点弹窗
    
    * pull
    
    * aaa
    
    * 基本没问题
    
    * 蛤?
    
    * 资质认证
    
    * 头像显示问题
    
    * 注册好了
    
    * 抢单列表
    
    * 抢单列表第一次修订版
    
    * 报价

---
+++ PerseusShelfUnion/PerseusShelfUnion/Model/TakeOrderDetails.swift
@@ -0,0 +1,73 @@
+//
+//  TakeOrderDetails.swift
+//  PerseusShelfUnion
+//
+//  Created by 123 on 16/12/26.
+//  Copyright © 2016年 XHVolunteeriOS. All rights reserved.
+//
+
+import Foundation
+class Model_TakeOrderDetails: NSObject {
+    // 请求
+    class Requesting: NSObject {
+        var RobOrderID: Int?
+        init(RobOrderID: Int?) {
+            self.RobOrderID = RobOrderID
+        }
+    }
+    
+    // 响应
+    class Response: NSObject {
+        var InsAtticLayer: String?
+        var InsBeamHgh: String?
+        var InsHeight: String?
+        var InsName: String?
+        var InsFork: String?
+        var InsCycle: String?
+        var InsPlace: String?
+        var InsMoney: String?
+        var Weight: String?
+        var InsPhone: String?
+        var InsRemarks: String?
+        var StartTime: String?
+        var Structure: String?
+        var Tonnage: String?
+        var InsType: String?
+        init(InsAtticLayer: String?,
+             InsBeamHgh: String?,
+             InsHeight: String?,
+             InsName: String?,
+             InsFork: String?,
+             InsCycle: String?,
+             InsPlace: String?,
+             InsMoney: String?,
+             Weight: String?,
+             InsPhone: String?,
+             InsRemarks: String?,
+             StartTime: String?,
+             Structure: String?,
+             Tonnage: String?,
+             InsType: String?) {
+            self.InsAtticLayer = InsAtticLayer
+            self.InsBeamHgh = InsBeamHgh
+            self.InsHeight = InsHeight
+            self.InsName = InsName
+            self.InsFork = InsFork
+            self.InsCycle = InsCycle
+            self.InsPlace = InsPlace
+            self.InsMoney = InsMoney
+            self.Weight = Weight
+            self.InsPhone = InsPhone
+            self.InsRemarks = InsRemarks
+            self.StartTime = StartTime
+            self.Structure = Structure
+            self.Tonnage = Tonnage
+            self.InsType = InsType
+        }
+    }
+    
+    enum CodeType: Int {
+        case 未抢 = 0
+        case 已抢 = 1
+    }
+}
